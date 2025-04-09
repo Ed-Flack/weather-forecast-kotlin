5ee8dc4692e1ff4741c6b5268af1507bd97ed0bb
Ed <mredwardflack@gmail.com>
2025-04-09 16:13:29 +0100

    MORE TESTS

--- src/main/kotlin/com/teamoptimization/AcmeForecastClient.kt
+++ src/main/kotlin/com/teamoptimization/AcmeForecastClient.kt
@@ -1,15 +1,13 @@
 package com.teamoptimization
 
-import org.http4k.client.JavaHttpClient
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
 
 class AcmeForecastClient(private val handler: HttpHandler) : BaseClient {
-
     override fun acmeForecast(day: String, place: String): AcmeForecastingClientResult =
         handler(
-            Request(Method.GET, "https://pqjbv9i19c.execute-api.eu-west-2.amazonaws.com/api/forecast?place=$place&day=$day")
+            Request(Method.GET, "/api/forecast?place=$place&day=$day")
         ).let { response ->
             if (response.status.successful) {
                 println(response)
