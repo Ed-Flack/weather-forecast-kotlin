1cf7e783039500c891b9b54e879a4e481908572a
Ed <mredwardflack@gmail.com>
2025-04-10 13:18:18 +0100

    refactor

--- src/main/kotlin/com/teamoptimization/AcmeForecastFake.kt
+++ src/main/kotlin/com/teamoptimization/AcmeForecastFake.kt
@@ -2,16 +2,16 @@ package com.teamoptimization
 
 class AcmeForecastFake : BaseClient {
 
-    private var possibleWeatherMap: Map<String, List<AcmeForecastingClientResult>> = mutableMapOf()
+    private var possibleWeatherMap: MutableMap<String, List<AcmeForecastingClientResult>> = mutableMapOf()
 
     init {
-        (possibleWeatherMap as MutableMap<String, List<AcmeForecastingClientResult>>)["Oxford"] = mutableListOf(
+        possibleWeatherMap["Oxford"] = mutableListOf(
             AcmeForecastingClientResult("4", "10", "Cold and Rainy"),
             AcmeForecastingClientResult("10", "23", "Sunny"),
             AcmeForecastingClientResult("10", "15", "Stormy"),
             AcmeForecastingClientResult("-5", "0", "Snowy")
         )
-        (possibleWeatherMap as MutableMap<String, List<AcmeForecastingClientResult>>)["London"] = mutableListOf(
+        possibleWeatherMap["London"] = mutableListOf(
             AcmeForecastingClientResult("4", "25", "Stabby"),
             AcmeForecastingClientResult("0", "7", "Stinky"),
             AcmeForecastingClientResult("10", "15", "Smoggy"),
